bbd7493838440fb92738d70b2f22b35457145fdb
kasu naga veera venkata srirama anudeep <90023127+anudee796@users.noreply.github.com>
2022-06-11 11:34:14 +0530

    Create if-else1.cpp

---
+++ conditionals/if-else1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    //variable is used to store value
+    //variabe value may change during program execution
+    //variable must be declared before using in our program
+    if(m>n){
+        cout<<"m is gtr n"<<endl;
+    }else{
+        if(n>m){
+            cout<<"n is gtr m"<<endl;
+        }else{
+            cout<<"they are equal"<<endl;
+        }
+    }
+}
+
+
+
+
+
