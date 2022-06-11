6f65bcb61fa91002e623a34c2db9a8d56a2d30b9
kasu naga veera venkata srirama anudeep <90023127+anudee796@users.noreply.github.com>
2022-06-11 11:17:16 +0530

    Create if.cpp

---
+++ conditionals/if.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n=10;
+  int m=20;
+  if(n>m){
+    cout<<"n is gtr m"<<endl;
+  }
+  if(m>n){
+    cout<<"m is gtr n"<<endl;
+  }
+  
+}
